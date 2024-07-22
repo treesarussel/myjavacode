ad93b94e7da314e2aba338fb3d14b6c98dd8453c
Treesa Russel <treesarussel@gmail.com>
2024-07-22 16:26:53 +0530

    narrowing example

---
+++ NarrowingDouble_to_float.java
@@ -0,0 +1,15 @@
+package assignment;
+//convert double value to float
+public class NarrowingDouble_to_float
+{
+
+	public static void main(String[] args) {
+		//double into float
+		double a1 =456.56567890789077890;
+		float b1 =(float)a1;
+		System.out.println(b1);
+		
+
+	}
+
+}
