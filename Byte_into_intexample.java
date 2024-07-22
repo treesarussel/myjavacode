ecfb9e9371b88d10645113ce5665a419b6bd599c
Treesa Russel <treesarussel@gmail.com>
2024-07-22 16:14:18 +0530

    type casting

---
+++ Byte_into_intexample.java
@@ -0,0 +1,14 @@
+package assignment;
+//byte into int
+//Write a program give byte as value 127 and type cast byte to int
+public class Byte_into_intexample {
+
+	public static void main(String[] args) {
+		byte a=127;
+		int newno= (int)(45678);//widening explicit manner
+		System.out.println(newno);
+		
+
+	}
+
+}
