f2f56988f3a513b8637e9cd836a4c2dc1277f767
Treesa Russel <treesarussel@gmail.com>
2024-08-18 16:46:42 +0530

    continue keyword

---
+++ Continue_keyword.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class Continue_keyword {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++)//write for loop to print 1 to 10
+		{
+			if(i==7) 
+			{
+				continue;// continue statement can be used to skip this particular i==7 iteration
+			
+			}
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
