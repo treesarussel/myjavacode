6b14032d32406b7ab77d47eed0a1d8f2e251f194
Treesa Russel <treesarussel@gmail.com>
2024-08-18 23:47:38 +0530

    all browsers launch

---
+++ Launch_Allbrowsers.java
@@ -0,0 +1,17 @@
+//Launch all the web browsers available on your system through selenium code
+package seleniumpackage;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Launch_Allbrowsers {
+
+	public static void main(String[] args) {
+		ChromeDriver c1=new ChromeDriver();
+		FirefoxDriver f1=new FirefoxDriver();
+		EdgeDriver e1=new EdgeDriver();
+
+	}
+
+}
