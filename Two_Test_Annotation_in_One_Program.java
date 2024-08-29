b5862be4b70900e931e9d174633e6f443ecc5e0b
Treesa Russel <treesarussel@gmail.com>
2024-08-29 16:27:38 +0530

    2 test annotations in one program

---
+++ Two_Test_Annotation_in_One_Program.java
@@ -0,0 +1,19 @@
+package selenium.packkage.testng;
+
+import org.testng.annotations.Test;
+
+public class Two_Test_Annotation_in_One_Program {
+
+	@Test
+	public void Test_one()
+	{
+		System.out.println("This is test one");
+	}
+@Test
+	public void Test_two()
+	{
+		System.out.println("this is test two");
+		
+	}
+
+}
